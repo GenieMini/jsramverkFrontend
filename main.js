c9aef51971501ecadd96dfefc5f186cc42a891cd
JockeTS <jotn22@student.bth.se>
2023-08-31 15:31:07 +0200

    added comments for code understanding

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 function renderMainView() {
     let container = document.getElementById("container");
 
+    // Remove existing children from container before recreating them
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 
-    container.innerHTML = `<div class="delayed">
-                <h1>Försenade tåg</h1>
+    container.innerHTML = `
+    <div class="delayed">
+        <h1>Försenade tåg</h1>
 
-                <div id="delayed-trains" class="delayed-trains"></div>
-            </div>
-            <div id="map" class="map"></div>`;
+        <div id="delayed-trains" class="delayed-trains"></div>
+    </div>
+
+    <div id="map" class="map"></div>`;
 
     const socket = io("http://localhost:1337");
 
@@ -23,6 +26,7 @@ function renderMainView() {
 
     let markers = {};
 
+    // Update markers with new data when received from event source (trains model)
     socket.on("message", (data) => {
         if (markers.hasOwnProperty(data.trainnumber)) {
             let marker = markers[data.trainnumber]
@@ -37,6 +41,7 @@ function renderMainView() {
 
     let delayed = document.getElementById("delayed-trains");
 
+    // GET delayed data from /delayed route and render it
     fetch("http://localhost:1337/delayed")
         .then((response) => response.json())
         .then(function(result) {
@@ -44,8 +49,10 @@ function renderMainView() {
         });
 }
 
+// Create table of delay items from delayed data (delayed model)
 function renderDelayedTable(data, table) {
     data.forEach((item) => {
+        // 1 row/div per data item
         let element = document.createElement("div");
 
         element.innerHTML = `
@@ -64,10 +71,12 @@ function renderDelayedTable(data, table) {
             renderTicketView(item);
         });
 
+        // Append created element to table
         table.appendChild(element);
     });
 }
 
+// Calculate delay in minutes
 function outputDelay(item) {
     let advertised = new Date(item.AdvertisedTimeAtLocation);
     let estimated = new Date(item.EstimatedTimeAtLocation);
@@ -77,14 +86,17 @@ function outputDelay(item) {
     return Math.floor(diff / (1000 * 60)) + " minuter";
 }
 
+// Create a ticket view for clicked delay item
 function renderTicketView(item) {
     let container = document.getElementById("container");
     let newTicketId = 0;
 
+    // Remove existing children from container before recreating them
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 
+    // Create locationString from item data
     var locationString = "";
     if (item.FromLocation) {
          locationString = `<h3>Tåg från ${item.FromLocation[0].LocationName} till ${item.ToLocation[0].LocationName}. Just nu i ${item.LocationSignature}.</h3>`;
@@ -114,21 +126,25 @@ function renderTicketView(item) {
     let newTicketForm = document.getElementById("new-ticket-form");
     let oldTickets = document.getElementById("old-tickets");
 
+    // Set event listener for back button (renderMainView)
     backButton.addEventListener("click", function(event) {
         event.preventDefault();
 
         renderMainView();
     });
 
+    // Set event listener for new ticket form
     newTicketForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
+        // Create new ticket from code selected by user and current item data
         var newTicket = {
             code: reasonCodeSelect.value,
             trainnumber: item.OperationalTrainNumber,
             traindate: item.EstimatedTimeAtLocation.substring(0, 10),
         };
 
+        // POST new ticket to /tickets route (inserts new ticket into db)
         fetch("http://localhost:1337/tickets", {
             body: JSON.stringify(newTicket),
             headers: {
@@ -138,15 +154,19 @@ function renderTicketView(item) {
         })
             .then((response) => response.json())
             .then((result) => {
+                // Re-render ticket view to include newly created ticket
                 renderTicketView(item);
             });
     });
 
+    // GET existing tickets from /tickets route
+    // Render all tickets in db under "Befintliga ärenden"
     fetch("http://localhost:1337/tickets")
         .then((response) => response.json())
         .then((result) => {
             var lastId = result.data[1] ? result.data[1].id : 0;
 
+            // Set correct newTicketId
             newTicketId = lastId + 1;
 
             let newTicketIdSpan = document.getElementById("new-ticket-id");
@@ -163,7 +183,8 @@ function renderTicketView(item) {
         });
 
 
-
+    // GET reason codes from /codes route -> codes model
+    // Populate reasonCodeSelect with result
     fetch("http://localhost:1337/codes")
         .then((response) => response.json())
         .then((result) => {
@@ -176,8 +197,7 @@ function renderTicketView(item) {
                 reasonCodeSelect.appendChild(element);
             });
         });
-
-
 }
 
+// Run on start?
 renderMainView();
